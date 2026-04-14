6fb39624769e1193fee9ef763a69fbc580cf5831
Cleber Fernandes de Lima <cfleli2025@usp.br>
2026-04-14 10:50:39 -0300

    led_laranja_e_foto

--- src/main.c
+++ src/main.c
@@ -7,32 +7,23 @@
 // Define o valor do registrador MOD do TPM para configurar o período do PWM
 #define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 // Valores de duty cycle correspondentes a diferentes larguras de pulso
-uint16_t duty_50  = TPM_MODULE/2;       // 50% de duty cycle
+uint16_t duty_Red  = TPM_MODULE*0.700;       
+uint16_t duty_Green  = TPM_MODULE*0.900;       
 
 int main(void)
 {
-    // Inicializa o módulo TPM2 com:
-    // - base do TPMx
-    // - fonte de clock PLL/FLL (TPM_CLK)
-    // - valor do registrador MOD
-    // - tipo de clock (TPM_CLK)
-    // - prescaler de 1 a 128 (PS)
-    // - modo de operação EDGE_PWM
     pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
     // Inicializa o canal 0 do TPM2 para gerar sinal PWM na porta GPIOB_18
     // - modo TPM_PWM_H (nível alto durante o pulso)
-    // pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); // Vermelho
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); // vermelho
     pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); // Verde
 
     // Loop infinito
     while(1)
     {
-        pwm_tpm_CnV(TPM2, 1, duty_50);
-        k_msleep(TEMPO);
-        pwm_tpm_CnV(TPM2, 1, 1000);
-        k_msleep(TEMPO);
+        pwm_tpm_CnV(TPM2, 0, duty_Red);
+        pwm_tpm_CnV(TPM2, 1, duty_Green);
     }
-
     return 0;
 }   
