6d2d5037a3302cd251ab5742b62661437179ce6a
Cleber Fernandes de Lima <cfleli2025@usp.br>
2026-04-15 10:13:18 -0300

    Primeiro_teste_para_motores

--- src/main.c
+++ src/main.c
@@ -3,12 +3,11 @@
 #include <zephyr/drivers/gpio.h>       // API para controle de pinos de entrada/saída (GPIO)
 #include <pwm_z42.h>                   // Biblioteca personalizada com funções de controle do TPM (Timer/PWM Module)
 
-#define TEMPO 1000 // 1 segundo
+#define TEMPO 2000 // 2 segundo
 // Define o valor do registrador MOD do TPM para configurar o período do PWM
 #define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 // Valores de duty cycle correspondentes a diferentes larguras de pulso
-uint16_t duty_Red  = TPM_MODULE*0.700;       
-uint16_t duty_Green  = TPM_MODULE*0.900;       
+uint16_t duty_Red  = TPM_MODULE*0.500;             
 
 int main(void)
 {
@@ -18,15 +17,17 @@ int main(void)
     // - modo TPM_PWM_H (nível alto durante o pulso)
     pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); // vermelho
     pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); // Verde
-
+    //Pinos dos motores (PTB2 e PTB3)
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 12); //PTB2 - pino 12
+    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 13); //PTB3 - pino 13
     // Loop infinito
     while(1)
     {
         pwm_tpm_CnV(TPM2, 0, duty_Red);
-        pwm_tpm_CnV(TPM2, 1, duty_Green);
+        pwm_tpm_CnV(TPM2, 1 , 1000);
         k_msleep(TEMPO);
         pwm_tpm_CnV(TPM2, 0, 1000);
-        pwm_tpm_CnV(TPM2, 1, 1000);
+        pwm_tpm_CnV(TPM2, 1, duty_Red);
         k_msleep(TEMPO);
     }
     return 0;
